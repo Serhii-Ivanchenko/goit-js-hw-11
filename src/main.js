f0172bfde4c0bd1ca5f91349a221e713c9ed6798
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-04-05 09:24:32 +0300

    exports/imports done

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.js-search-form ');
-const btn = document.querySelector('.search-button');
-const input = document.querySelector('.search-input');
-const gallery = document.querySelector('ul.gallery');
-const loader = document.querySelector('.loader');
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '43222860-c920ce4922a75b9f5ac3c35c2';
+import { getData } from './js/pixabay-api.js';
+import { createMarkUp } from './js/render-functions.js';
+import { form, input, gallery } from './js/refs.js';
+import { loaderShow } from './js/loader.js';
+
 let searchInput = '';
 
 form.addEventListener('submit', onFormSubmit);
@@ -20,12 +18,17 @@ function onFormSubmit(event) {
   gallery.innerHTML = '';
 
   if (input.value.trim() === '') {
-    return alert('Fill the field!');
+    return iziToast.error({
+      title: '',
+      message: 'The field can not be empty!!!',
+      position: 'topCenter',
+      timeout: 3000,
+      pauseOnHover: false,
+    });
   }
 
   searchInput = input.value;
-
-  loader.classList.toggle('visible');
+  loaderShow();
 
   getData(searchInput)
     .then(data => {
@@ -35,70 +38,20 @@ function onFormSubmit(event) {
           message:
             'Sorry, there are no images matching your search query. Please try again!',
           position: 'topRight',
+          timeout: 3000,
+          pauseOnHover: false,
         });
-        loader.classList.toggle('visible');
+        loaderShow();
       } else {
         gallery.insertAdjacentHTML('beforeend', createMarkUp(data.hits));
         const photosGallery = new SimpleLightbox('.gallery a', {
           captionsData: 'alt',
           captionDelay: 250,
-          overlayOpacity: 0.8,
         });
         photosGallery.refresh();
-        loader.classList.toggle('visible');
+        loaderShow();
       }
     })
     .catch(error => console.log(error))
     .finally(() => form.reset());
-}
-
-function getData(str) {
-  const parameters = new URLSearchParams({
-    key: API_KEY,
-    q: str,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-  });
-
-  return fetch(`${BASE_URL}?${parameters}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
-
-function createMarkUp(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<li class="gallery-item">
-        <div class="thumb">
-            <a class="gallery-link" href="${largeImageURL}">
-              <img
-                class="gallery-image"
-                src="${webformatURL}"
-                alt="${tags}"
-              />
-            </a>
-              <ul class="description-list">
-                <li class="description-items"><span class="accent">Likes </span>${likes}</li>
-                <li class="description-items"><span class="accent">Views </span>${views}</li>
-                <li class="description-items"><span class="accent">Comments </span>${comments}</li>
-                <li class="description-items"><span class="accent">Downloads </span>${downloads}</li>
-              </ul>
-        </div>
-      </li>`
-    )
-    .join('');
 }
